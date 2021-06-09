ef919d1da20f50e645f8b4e2b611f63017b3f126
Adwait Kulkarni <adwait.kul.2018@gmail.com>
2021-06-09 19:54:43 +0530

    Added label to the existing frame

--- Simple Java GUI/Main.java
+++ Simple Java GUI/Main.java
@@ -3,12 +3,19 @@ import javax.swing.*;
 
 class Main {
   public static void main(String[] args) {
+    JLabel label = new JLabel();
     JFrame frame = new JFrame();
 
+    label.setText("Hello there!");
+    label.setBackground(new Color(0, 0, 0));
+    label.setForeground(new Color(255, 255, 255));
+    label.setOpaque(true);
+    
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(420, 420);
     frame.setVisible(true);
     frame.setTitle("First GUI program");
     frame.setResizable(false);
+    frame.add(label);
   }
 }
