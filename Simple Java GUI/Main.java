9a0f44af07e7be41384ec8d44193a1e68172a56b
Adwait Kulkarni <adwait.kul.2018@gmail.com>
2021-06-09 21:01:35 +0530

    Added ImageIcon and formatting

--- Simple Java GUI/Main.java
+++ Simple Java GUI/Main.java
@@ -1,21 +1,32 @@
 import java.awt.Color;
+import java.awt.Font;
 import javax.swing.*;
 
 class Main {
   public static void main(String[] args) {
     JLabel label = new JLabel();
     JFrame frame = new JFrame();
+    ImageIcon image = new ImageIcon("testImage.jpg");
 
-    label.setText("Hello there!");
+    label.setIcon(image);
+    label.setText("The Iconic Trio");
+    label.setHorizontalTextPosition(JLabel.CENTER);
+    label.setVerticalTextPosition(JLabel.TOP);
+    label.setVerticalAlignment(JLabel.CENTER);
+    label.setHorizontalAlignment(JLabel.CENTER);
+    label.setBounds(100, 100, 250, 250);
+    label.setIconTextGap(15);
+    label.setFont(new Font("Elegant", Font.ITALIC, 20));
     label.setBackground(new Color(0, 0, 0));
     label.setForeground(new Color(255, 255, 255));
     label.setOpaque(true);
     
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(420, 420);
+    frame.setSize(500, 500);
     frame.setVisible(true);
     frame.setTitle("First GUI program");
     frame.setResizable(false);
     frame.add(label);
+    frame.pack();
   }
 }
