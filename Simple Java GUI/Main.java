bfee20e19749538f9f4ad32c4233209ae079f917
Adwait Kulkarni <adwait.kul.2018@gmail.com>
2021-06-09 19:49:20 +0530

    Created the main frame

---
+++ Simple Java GUI/Main.java
@@ -0,0 +1,14 @@
+import java.awt.Color;
+import javax.swing.*;
+
+class Main {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame();
+
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(420, 420);
+    frame.setVisible(true);
+    frame.setTitle("First GUI program");
+    frame.setResizable(false);
+  }
+}
